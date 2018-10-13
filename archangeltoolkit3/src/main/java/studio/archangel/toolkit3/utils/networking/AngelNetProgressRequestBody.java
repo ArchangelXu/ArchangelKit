e766145500d001ff6b45cf60039f3d2e09dfe6bb
ZzT <zhengtian@veer.tv>
2018-10-13 10:37:44 +0800

    bug: 修复上传进度不对，buffer 内存爆掉的问题

--- archangeltoolkit3/src/main/java/studio/archangel/toolkit3/utils/networking/AngelNetProgressRequestBody.java
+++ archangeltoolkit3/src/main/java/studio/archangel/toolkit3/utils/networking/AngelNetProgressRequestBody.java
@@ -1,91 +1,57 @@
 package studio.archangel.toolkit3.utils.networking;
 
-import android.content.ContentResolver;
-import android.content.Context;
-import android.database.Cursor;
-import android.net.Uri;
-import android.provider.OpenableColumns;
-
-import java.io.File;
 import java.io.IOException;
 
 import okhttp3.MediaType;
 import okhttp3.RequestBody;
-import okhttp3.internal.Util;
 import okio.Buffer;
 import okio.BufferedSink;
+import okio.ForwardingSink;
 import okio.Okio;
-import okio.Source;
-import studio.archangel.toolkit3.AngelApplication;
-import studio.archangel.toolkit3.utils.Logger;
+import okio.Sink;
 
 /**
  * Created by Administrator on 2015/10/16.
  */
 public class AngelNetProgressRequestBody extends RequestBody {
 
-	//	private final File file;
-	private final Uri uri;
+	private RequestBody requestBody;
+	private BufferedSink bufferedSink;
 	private final AngelNetCallBack listener;
-	private final String contentType;
-	private AngelNet handler;
-	long length;
 
-	public AngelNetProgressRequestBody(Uri uri, String contentType, AngelNetCallBack listener, AngelNet angelNet) {
-//		this.file = file;
-		this.uri = uri;
-		this.contentType = contentType;
+	public AngelNetProgressRequestBody(RequestBody requestBody, AngelNetCallBack listener) {
+		this.requestBody = requestBody;
 		this.listener = listener;
-		handler = angelNet;
-		String scheme = uri.getScheme();
-		if (scheme.equals(ContentResolver.SCHEME_FILE)) {
-			File file = new File(uri.getPath());
-			length = file.length();
-		} else if (scheme.equals(ContentResolver.SCHEME_CONTENT)) {
-			Cursor cursor = AngelApplication.getInstance().getContentResolver().query(uri, null, null, null, null);
-			cursor.moveToFirst();
-			length = cursor.getLong(cursor.getColumnIndex(OpenableColumns.SIZE));
-			cursor.close();
-		}
 	}
 
 	@Override
-	public long contentLength() {
-		return length;
+	public long contentLength() throws IOException {
+		return requestBody.contentLength();
 	}
 
 	@Override
 	public MediaType contentType() {
-		return MediaType.parse(contentType);
+		return requestBody.contentType();
 	}
 
 	@Override
 	public void writeTo(BufferedSink sink) throws IOException {
-		Source source = null;
-		try {
-			source = Okio.source(AngelApplication.getInstance().getContentResolver().openInputStream(uri));
-			long total = 0;
-			long read;
-			Buffer buffer = sink.buffer();
-			while ((read = source.read(buffer, 32 * 1024)) != -1) {
-				total += read;
-//				sink.flush();
-//				Logger.out("uploading:" + total + "/" + contentLength());
-				final long finalTotal = total;
-				if (listener != null) {
-					if (listener instanceof AngelNetProgressCallBack) {
-						handler.execute(new Runnable() {
-							@Override
-							public void run() {
-								((AngelNetProgressCallBack) listener).onProgress(finalTotal, contentLength(), false);
-							}
-						});
-					}
-				}
+		bufferedSink = Okio.buffer(sink(sink));
+		requestBody.writeTo(bufferedSink);
+		bufferedSink.flush();
+	}
+
+	private Sink sink(Sink delegate) {
+		return new ForwardingSink(delegate) {
+
+			private long uploadedSize;
+
+			@Override
+			public void write(Buffer source, long byteCount) throws IOException {
+				super.write(source, byteCount);
+				uploadedSize += byteCount;
+				((AngelNetProgressCallBack) listener).onProgress(uploadedSize, contentLength(), false);
 			}
-			sink.flush();
-		} finally {
-			Util.closeQuietly(source);
-		}
+		};
 	}
 }
